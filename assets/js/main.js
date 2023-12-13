4966fe2eb513edc90e28d5dc8d422f9db7917142
AhissouOnel <onelahissou7@gmail.com>
2023-12-13 17:07:19 +0100

    Projet professionnelle

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ window.onload = () =>{
 
     const currentUrl = window.location.href;
     const menuItems = document.querySelectorAll('.menu-item');
-
     menuItems.forEach(item => {
         const menuItem = item.querySelector('a');
         const linkUrl = menuItem.getAttribute('href');
@@ -17,10 +16,34 @@ window.onload = () =>{
 
 
 
-
+    var content = document.querySelectorAll('.content')
     var navIcon = document.getElementById('nav-icon')
     var navLeft = document.querySelector('.left')
     navIcon.onclick = () =>{
         navLeft.classList.toggle('block')
+        navLeft.style.zIndex = 998
+        content.style.zIndex = 997
+        content.style.backgroundImage = ("/assets/images/Home-background/Home_background.jpg")
+        content.style
     }
+ 
+
+
+
+    var statusFirst = document.querySelector('.status-first')
+    var statusSecond = document.querySelector('.status-second')
+
+    statusFirst.style.display = "none"
+    statusSecond.style.display = "none"
+    setTimeout(() => {
+        setInterval(() => {
+            statusFirst.style.display = "block"
+            statusSecond.style.display = "none"
+        }, 4000);
+        setInterval(() => {
+            statusFirst.style.display = "none"
+            statusSecond.style.display = "block"
+        }, 8000);
+    }, 9000);
+    
 }
